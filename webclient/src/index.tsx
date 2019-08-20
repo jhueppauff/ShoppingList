7b39eaac7f4a00b436f7cf27593e46731770878d
Julian Hüppauff <julian@hueppauff.com>
2019-08-20 20:32:19 +0200

    added b2c

--- webclient/src/index.tsx
+++ webclient/src/index.tsx
@@ -3,10 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+const authentication = require('@kdpw/msal-b2c-react');
+//import authentication from '@kdpw/msal-b2c-react';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+authentication.initialize({
+    instance: 'https://login.microsoftonline.com/tfp/',
+    tenant: 'HueppauffB2C.onmicrosoft.com',
+    signInPolicy: 'B2C_1_b2s_1_susi',
+    applicationId: '739f43a1-d8fe-401e-97bc-3f07525e1292',
+    cacheLocation: 'sessionStorage',
+    scopes: ['user_impersonation'],
+    redirectUri: 'http://localhost:3000',
+    postLogoutRedirectUri: window.location.origin,
+});
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+authentication.run(() => {
+    ReactDOM.render(<App />, document.getElementById('root'));
+    serviceWorker.unregister();
+});

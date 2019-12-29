934c8e047e370ff6b6e7a5974f5331e707943195
Julian Hüpppauff <julian@hueppauff.info>
2019-12-29 19:47:29 +0100

    removed unused usings

--- ShoppingList/Data/ShoppingItemRequest.cs
+++ ShoppingList/Data/ShoppingItemRequest.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace ShoppingList.Data
+﻿namespace ShoppingList.Data
 {
     public class ShoppingItemRequest
     {
